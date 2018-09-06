8e263931bb8b58a7f69b8745e64a060d343c657d
wangzhang <912554887@qq.com>
2018-09-06 23:21:16 +0800

    update

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import sys
 import os
 import argparse
 
-sys.path.append(os.path.abspath('.') + './pytools')
+sys.path.append(os.path.abspath('.') + './pys')
 
 import version
 
+def version():
+    version.version('./release_note.txt')
+    sys.exit(-1)
+
 def main():
     parser = argparse.ArgumentParser('multi-chain usage')
-    parser.add_argument('-C', '--config', type=str, dest='config',
+    parser.add_argument('--config', type=str, dest='config',
                         help="input config file which in ini format")
     parser.add_argument("-V", "--version", action="store_true",
                         help="version of multi-chain")
-    parser.add_argument("--new_account", help="generate a new account")
     args = parser.parse_args()
-
-    for arg in args.__dict__.keys():
-        print(args.__dict__)
-        print(arg)
-        if args.__dict__['version']:
-            version.version('./release_note.txt')
-            sys.exit(-1)
-        elif arg == 'config':
-            print(args.__dict__[arg])
+    print(args)
+    for arg in args.__dict__:
+        if arg == 'version' and args.version:
+            version()
+        elif arg == 'config' and args.config is not None:
+            config_file = args.config
+            print(config_file)
 
 
 if __name__ == '__main__':
