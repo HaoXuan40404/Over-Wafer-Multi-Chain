1b4cd326b4a8e9316855f329dc4b0825fbdc8686
octopuswang <octopuswang@webank.com>
2018-09-13 14:20:12 +0800

    add 添加ansible的执行用户、推送路径配置对应的类

---
+++ pys/ansible.py
@@ -0,0 +1,24 @@
+class Ansible:
+    '''
+    ansible
+    '''
+    user = ''
+    dir = ''
+
+    def __repr__(self):
+        return '[user] %s, [dir] %s' % (Ansible.user, Ansible.dir)
+
+def set_user(user):
+    Ansible.user = user
+
+def set_dir(dir):
+    Ansible.dir = dir
+
+def ansible_test():
+    ae = Ansible()
+    set_user('app')
+    set_dir('dir')
+    print(ae)
+
+if __name__ == '__main__':
+    ansible_test()
