60166bb24271b17cefb1d9d2ca80ffce14c1fa0c
wangzhang <912554887@qq.com>
2018-09-16 21:04:00 +0800

    update

--- pys/node/temp_node.py
+++ pys/node/temp_node.py
@@ -17,11 +17,15 @@ def temp_node_build(dir, port):
     logger.info('build temp node, dir => ' + dir)
     os.makedirs(dir + '/temp')
     shutil.copytree(path.get_path() + '/tpl/web3sdk', dir + '/temp/web3sdk')
+
     shutil.copytree(path.get_path() + '/tpl/temp_node', dir + '/temp/node')
+
+    shutil.copy(dir + '/temp/node/sdk/ca.crt', dir + '/temp/web3sdk/conf')
+    shutil.copy(dir + '/temp/node/sdk/client.keystore', dir + '/temp/web3sdk/conf')
     cfg_json = config.build_config_json('12345', port.get_rpc_port(), port.get_p2p_port(), port.get_channel_port())
     with open(dir + '/temp/node/config.json',"w+") as f:
             f.write(cfg_json)
-            
+
     old = 'NODE@HOSTIP'
     new = 'node0@127.0.0.1:%d' % port.get_p2p_port()
     utils.replace(dir + '/temp/web3sdk/conf/applicationContext.xml', old, new)
