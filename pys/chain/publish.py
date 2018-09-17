b5c86c7cb8f60f5be9cd56226e0a23d8084ed82d
HaoXuan40404 <asherli@asherli-NB1.webank.com>
2018-09-17 17:13:53 +0800

    update file

--- pys/chain/publish.py
+++ pys/chain/publish.py
@@ -13,7 +13,7 @@ def publish_server(chain_id, chain_version):
         return
     for host in os.listdir(dir):
         if utils.valid_ip(host):
-            ansible.mdir_module(host, dest_dir + chain_id)
+            ansible.mkdir_module(host, dest_dir + chain_id)
             ansible.copy_module(host, dir  + '/' + host  + '/', dest_dir + chain_id + '/')
         else:
             print("wrong host_ip!",host)
