99bb3317dac186da8998ea6c73ad38a2633c9eb9
wangzhang <912554887@qq.com>
2018-09-17 09:02:40 +0800

    update

---
+++ tpl/temp_node/export.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dirpath="$(cd "$(dirname "$0")" && pwd)"
+    cd $dirpath
+    curdir=$PWD
+    node=$(basename ${curdir})
+    ./fisco-bcos --genesis ${curdir}/genesis.json  --config ${curdir}/config.json --export-genesis $1  > ${curdir}/fisco-bcos.log 2>&1
