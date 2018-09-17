3a42720c6c357ad36536fe29a4e8505dcb8d508a
octopuswang <octopuswang@webank.com>
2018-09-17 20:08:21 +0800

    update

--- pys/chain/opr.py
+++ pys/chain/opr.py
@@ -1,41 +1,35 @@
-from pys.chain import data
 import os
-from pys import ansible
-from pys import utils
+
+from pys import ansible, utils
+from pys.chain import data
 from pys.log import logger
 
-dest_dir = '~/data/' 
 
-def start_server(chain_id, chain_version):
-    dir = data.Data().dir(chain_id,chain_version)
+def start_server(chain_id, chain_version, dest_dir):
+    dir = data.Data().dir(chain_id, chain_version)
     if not os.path.isdir(dir):
         logger.warn('not this node, your node is at  ' + dir)
         return
     for host in os.listdir(dir):
         if utils.valid_ip(host):
-            ansible.start_module(host, dest_dir + chain_id + '/')
-        else:
-            logger.debug('cant start node at  ' + host)
+            ansible.start_module(host, dest_dir + '/' + chain_id)
 
-def stop_server(chain_id, chain_version):
-    dir = data.Data().dir(chain_id,chain_version)
+
+def stop_server(chain_id, chain_version, dest_dir):
+    dir = data.Data().dir(chain_id, chain_version)
     if not os.path.isdir(dir):
         logger.warn('not this node, your node is at  ' + dir)
         return
     for host in os.listdir(dir):
         if utils.valid_ip(host):
-            ansible.stop_module(host, dest_dir + chain_id + '/')
-        else:
-            logger.debug('cant stop node at  ' + host)
+            ansible.stop_module(host, dest_dir + '/' + chain_id)
 
 
-def check_server(chain_id, chain_version):
-    dir = data.Data().dir(chain_id,chain_version)
+def check_server(chain_id, chain_version, dest_dir):
+    dir = data.Data().dir(chain_id, chain_version)
     if not os.path.isdir(dir):
         logger.warn('not this node, your node is at  ' + dir)
         return
     for host in os.listdir(dir):
         if utils.valid_ip(host):
-            ansible.check_module(host, dest_dir + chain_id + '/')
-        else:
-            logger.debug('cant stop node at  ' + host)
+            ansible.check_module(host, dest_dir + '/' + chain_id)
