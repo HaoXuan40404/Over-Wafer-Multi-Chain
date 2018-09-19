617544e116ec211961cc93f3404206b0f4aa4e86
haoxuan40404 <444649358@qq.com>
2018-09-19 14:45:24 +0800

    update

--- pys/checktools/check_environment.py
+++ pys/checktools/check_environment.py
@@ -2,7 +2,6 @@ import os
 
 from pys import ansible, utils
 from pys.chain import data
-<<<<<<< HEAD
 from pys.chain import meta
 from pys.log import logger
 
@@ -14,17 +13,5 @@ def check_environment(chain_id):
     for k,v in mm.get_nodes().items():
         logger.debug('host ip is ' + k)
         ansible.environment_module(k, ansible.get_dir() + '/' + chain_id)
-=======
-from pys.log import logger
 
-
-def check_environment(chain_id, chain_version):
-    dir = data.Data().dir(chain_id, chain_version)
-    if not os.path.isdir(dir):
-        logger.warn('check_environment error  ')
-        return
-    for host in os.listdir(dir):
-        if utils.valid_ip(host):
-            ansible.environment_module(host, ansible.get_dir() + '/' + chain_id)
->>>>>>> 0f634a1a0102382713c8547e9bc68598218afea3
 
