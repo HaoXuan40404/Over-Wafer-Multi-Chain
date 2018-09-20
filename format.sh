c44ec6462f1a6cbc713b286c86fe3795526b8e25
octopuswang <octopuswang@webank.com>
2018-09-20 14:12:18 +0800

    update

---
+++ format.sh
@@ -0,0 +1,3 @@
+find . -name "*.json"|  while read LINE; do  dos2unix $LINE; done
+find . -name "*.sh"|  while read LINE; do chmod +x $LINE; dos2unix $LINE; done
+find . -name "web3sdk"|  while read LINE; do chmod +x $LINE; dos2unix $LINE; done
