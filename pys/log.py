09de01917e9e891cbbe03ac2424a57e00c35d86a
octopuswang <octopuswang@webank.com>
2018-09-21 14:57:30 +0800

    update

--- pys/log.py
+++ pys/log.py
@@ -9,5 +9,7 @@ from pys import path
 # logging init
 logging.config.fileConfig(os.getcwd() + '/conf/logging.conf')
 logger = logging.getLogger('instance')
+consoler = logging.getLogger('console')
 # log start
 logger.info('init_logging init. ')
+consoler.info('init_logging init. ')
