0ffce3ecc19efbcc0c0cd6f7a3b433a9d287b263
haoxuan40404 <444649358@qq.com>
2018-09-28 10:49:14 +0800

    add portcheck

--- scripts/node/node_start.sh
+++ scripts/node/node_start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+source ../tools/port_check.sh
+
     dirpath="$(cd "$(dirname "$0")" && pwd)"
     cd $dirpath
     curdir=$PWD
     node=$(basename ${curdir})
+    channelPort=`cat ${curdir}/config.json | grep channelPort |grep -v grep|awk '{print $2}' | tr -cd "[0-9]"`
+    rpcport=`cat ${curdir}/config.json | grep rpcport |grep -v grep|awk '{print $2}' | tr -cd "[0-9]"`
+    p2pport=`cat ${curdir}/config.json | grep p2pport |grep -v grep|awk '{print $2}' | tr -cd "[0-9]"`
     ulimit -c unlimited
     weth_pid=`ps aux|grep "${curdir}/config.json"|grep -v grep|awk '{print $2}'`
     if [ ! -z $weth_pid ];then
         echo "${node} is running, pid is $weth_pid."
+    elif [check_port_use channelPort -o check_port_use  rpcport -o check_port_use p2pport];then
+        echo "port is using, please check the port."
     else
         echo "start ${node} ..."
         chmod a+x ../fisco-bcos
--- scripts/tools/port_check.sh
+++ scripts/tools/port_check.sh
@@ -15,3 +15,20 @@ function check_port()
         echo "$1 is not listening."
     fi
 }
+
+function check_port_use()
+{
+    type nc >/dev/null 2>&1
+    if [ $? -ne 0 ];then
+        echo "ERROR - nc is not installed."
+        return
+    fi
+
+    nc -z 127.0.0.1 $1 >/dev/null 2>&1
+    if [ $? -eq 0 ];then
+        echo "port $1 has been using."
+        return 1
+    else
+        return 0
+    fi
+}
