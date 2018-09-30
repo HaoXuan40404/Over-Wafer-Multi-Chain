a0aed92d8b70e67cd1621906d861793a24e549dc
haoxuan40404 <444649358@qq.com>
2018-09-30 10:17:51 +0800

    update mai

--- scripts/ssh_copy_add.sh
+++ scripts/ssh_copy_add.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cat ./conf/hosts.conf | while read user ip port passwd
 do
-sshpass -p ${passwd} ssh-copy-id  -i ~/.ssh/id_rsa -p ${port} ${user}@${ip}
+sshpass -p ${passwd} ssh-copy-id  -i ~/.ssh/id_rsa.pub -p ${port} ${user}@${ip}
 done
 ssh-agent bash
 ssh-add ~/.ssh/id_rsa
