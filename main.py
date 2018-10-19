e0edaf99b190510c89f2f5bd550fe154af37dc62
octopuswang <octopuswang@webank.com>
2018-10-19 15:30:04 +0800

    Fix main.py bug fix

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from pys.chain import publish
 
 
 def init():
- 
+
     # init pwd dir
     pwd = os.getcwd()
     sys.path.append(pwd + '/pys')
@@ -46,11 +46,12 @@ def usage():
         description='Description of multi-chain usage.')
     parser.add_argument('--version', action='store_true',
                         help='version of multi-chain')
-    parser.add_argument('--init_ansible', action='store_true', help='Output => Init ansible hosts need sudo.')                   
+    parser.add_argument('--init_ansible', action='store_true',
+                        help='Output => Init ansible hosts need sudo.')
     parser.add_argument('--build', nargs=2, metavar=('./config.conf or ./conf/',
                                                      'fisco_path'), help='Output => package. Build all package under directory ./data/chain/ according to the input.')
-    parser.add_argument('--expand', nargs='+', metavar=('./config.conf ', 'fisco_path', 'genesis.json path',
-                                                     'bootstapnodes.json path'), help='Output => package. Expand all package under directory ./data/chain/ according to the input.')
+    parser.add_argument('--expand', nargs='+', metavar=('./config.conf fisco_path genesis.json path bootstapnodes.json path'),
+                        help='Output => package. Expand all package under directory ./data/chain/ according to the input.')
     parser.add_argument('--publish', nargs='+', metavar=('chain_id:version eg.',
                                                          'chain_id_1:version_1 chain_id_2:version_1 chain_id_3:version_2.etc.'), help='Output => publish all package to servers')
     parser.add_argument('--check', nargs='+', metavar=('all or chain_id or',
@@ -64,10 +65,11 @@ def usage():
     parser.add_argument('--pkg_list', nargs='+', metavar=('all or chain_id or',
                                                           'chain_id_1 chain_id_2'), help='Output =>  list all build package info.')
     parser.add_argument('--pub_list', nargs='+', metavar=('all or chain_id or',
-                                                            'chain_id_1 chain_id_2'), help='Output =>  list all publish info.')
+                                                          'chain_id_1 chain_id_2'), help='Output =>  list all publish info.')
     parser.add_argument('--export', nargs=2, metavar=('chain_id',
-                                                        'dest_path'), help='Output =>  export publish package.')
-    parser.add_argument('--ls_port', nargs=1, metavar=('host_ip'), help='Output =>  ls all publish port')
+                                                      'dest_path'), help='Output =>  export publish package.')
+    parser.add_argument('--ls_port', nargs=1, metavar=('host_ip'),
+                        help='Output =>  ls all publish port')
     parser.add_argument('--telnet', nargs='+', metavar=('all or host_ip or',
                                                         'host_ip1 host_ip2'), help='Output => test ansible of servers is useful or not')
     parser.add_argument('--env_check', nargs='+', metavar=('all or host_ip'),
@@ -75,7 +77,7 @@ def usage():
     parser.add_argument('--cmd_push', nargs='+', metavar=('all:"cmd_1 cmd_2" or chain_id:"cmd_1 cmd_2" or',
                                                           'hostip:"cmd_1 cmd_2" or "chain:"./test.sh""'), help='Output =>  execute commands to Input.')
     parser.add_argument('--file_push', nargs='+', metavar=('all:scr_path:dest_path or chain_id:scr_path:dest_path or',
-                                                          'chain_id:scr_path:dest_path or host_ip:scr_path:dest_path'), help='Output =>  push a file to Input.')
+                                                           'chain_id:scr_path:dest_path or host_ip:scr_path:dest_path'), help='Output =>  push a file to Input.')
     parser.add_argument('--chainca', nargs=1, metavar=('./dir_chain_ca(SET)',),
                         help='Output => the cert of chain that set on the SET directory')
     parser.add_argument('--agencyca', nargs=3, metavar=('./dir_agency_ca(SET)',
@@ -180,11 +182,12 @@ def usage():
         consoler.info(' ls_port operation begin.')
         host_ip = args.ls_port[0]
         opr.ls_port(host_ip)
-        consoler.info(' ls_port operation end.')  
+        consoler.info(' ls_port operation end.')
     else:
         consoler.error(
             'invalid operation,  \"python main.py -h\" can be used to show detailed usage.')
     return 0
+
 
 def main():
     try:
@@ -193,6 +196,7 @@ def main():
         consoler.error(' OWMC init fault , %s', e)
     else:
         usage()
+
 
 if __name__ == '__main__':
     main()
