672934925c51a9bf9594f8d10b62b88c5e5ef8d7
octopuswang <octopuswang@webank.com>
2018-10-23 20:42:02 +0800

    update

--- pys/chain/expand.py
+++ pys/chain/expand.py
@@ -19,11 +19,66 @@ from pys.chain.port import AllChainPort
 
 from pys.node.fisco_version import Fisco
 
-
-def expand_cc(cc, fisco_path, genesisjson, bootstrapnodesjson):
+def expand_on_exist_chain(cc):
 
     chain = cc.get_chain()
     port = cc.get_port()
+    chain_id = chain.get_id()
+    chain_version = chain.get_version()
+
+    # expand on exist chain, check common、 genesis.json、 bootstrapnodes.json file exist.
+    if not os.path.exists(chain.data_dir() + '/common'):
+        raise MCError(' chain dir exist ,but common dir not exist, chain_id %s、chain_version %s' % (
+            chain_id, chain_version))
+    if not os.path.exists(chain.data_dir() + '/genesis.json'):
+        raise MCError(' chain dir exist ,but genesis.json not exist, chain_id %s、chain_version %s' % (
+            chain_id, chain_version))
+    if not os.path.exists(chain.data_dir() + '/bootstrapnodes.json'):
+        raise MCError(' chain dir exist ,but bootstrapnodes.json not exist, chain_id %s、and chain_version %s' % (
+            chain_id, chain_version))
+    
+    acp = AllChainPort()
+    # port check
+    for node in cc.get_nodes():
+        for index in range(node.get_node_num()):
+            # create dir for every node on the server
+            acp.port_conflicts(node.get_host_ip(), port.to_port(index))
+
+    fisco = Fisco(chain.data_dir() + '/' + 'common' + '/' + 'fisco-bcos')
+    
+    # expand install dir for every server
+    for node in cc.get_nodes():
+        try:
+            build.expand_host_dir(chain, node, port, fisco)
+        except Exception as e:
+            logger.error(' expand failed, chain id is %s, chain version is %s, exception is %s.',
+                     chain_id, chain_version, e)
+            raise MCError(' expand failed, chain id is %s, chain version is %s, exception is %s' % (
+            chain_id, chain_version, e))
+
+def expand_on_nonexist_chain(cc, fisco_path, genesisjson, bootstrapnodesjson):
+    
+    # check if fisco-bcos、genesis.json、bootstrapsnode.json exist.
+    if not os.path.exists(fisco_path):
+        raise MCError(
+            ' fisco bcos not exist, fisco bcos path is %s' % fisco_path)
+    if not os.path.exists(genesisjson):
+        raise MCError(
+            ' genesis.json not exist, genesis.json path is %s' % genesisjson)
+    if not os.path.exists(bootstrapnodesjson):
+        raise MCError(
+            ' bootstrapnodes.json not exist, bootstrapnodes.json path is %s' % bootstrapnodesjson)
+
+    chain = cc.get_chain()
+    port = cc.get_port()
+    chain_id = chain.get_id()
+    chain_version = chain.get_version()
+
+    # parser fisco-bcos version and check it.
+    fisco = Fisco(fisco_path)
+    if not fisco.is_13_version():
+        raise MCError(
+            ' fisco-bcos is not 1.3.x version, not support now, version is %s' % fisco)
 
     acp = AllChainPort()
     # port check
@@ -32,69 +87,27 @@ def expand_cc(cc, fisco_path, genesisjson, bootstrapnodesjson):
             # create dir for every node on the server
             acp.port_conflicts(node.get_host_ip(), port.to_port(index))
 
-    if os.path.exists(chain.data_dir()):
-        # expand on exist chain, check common、 genesis.json、 bootstrapnodes.json file exist.
-        if not os.path.exists(chain.data_dir() + '/common'):
-            raise MCError(' chain dir exist ,but common dir not exist, chain_id %s、chain_version %s' % (
-                chain.get_id(), chain.get_version()))
-        if not os.path.exists(chain.data_dir() + '/genesis.json'):
-            raise MCError(' chain dir exist ,but genesis.json not exist, chain_id %s、chain_version %s' % (
-                chain.get_id(), chain.get_version()))
-        if not os.path.exists(chain.data_dir() + '/bootstrapnodes.json'):
-            raise MCError(' chain dir exist ,but bootstrapnodes.json not exist, chain_id %s、and chain_version %s' % (
-                chain.get_id(), chain.get_version()))
+    try:
+        # create host dir
+        os.makedirs(chain.data_dir())
+        # copy genesis.json bootstrapnodes.json to chain dir.
+        shutil.copy(genesisjson, chain.data_dir() + '/')
+        shutil.copy(bootstrapnodesjson, chain.data_dir() + '/')
 
-        fisco = Fisco(chain.data_dir() + '/' + 'common' + '/' + 'fisco-bcos')
-        # expand install dir for every server
+        # create common dir
+        build.build_common_dir(chain, fisco)
+
+        # build install dir for every server
         for node in cc.get_nodes():
-            try:
-                build.expand_host_dir(chain, node, port, fisco)
-            except Exception as e:
-                continue
-    else:
-        try:
-            # check if fisco-bcos、genesis.json、bootstrapsnode.json exist.
-            if not os.path.exists(fisco_path):
-                raise MCError(
-                    ' fisco bcos not exist, fisco bcos path is %s' % fisco_path)
-            if not os.path.exists(genesisjson):
-                raise MCError(
-                    ' genesis.json not exist, genesis.json path is %s' % genesisjson)
-            if not os.path.exists(bootstrapnodesjson):
-                raise MCError(
-                    ' bootstrapnodes.json not exist, bootstrapnodes.json path is %s' % bootstrapnodesjson)
+            build.expand_host_dir(chain, node, port, fisco)
 
-             # parser fisco-bcos version and check it.
-            fisco = Fisco(fisco_path)
-            if not fisco.is_13_version():
-                logger.error(
-                    ' fisco-bcos is not 1.3.x version, not support now, version is %s', fisco)
-                raise MCError(
-                    ' fisco-bcos is not 1.3.x version, not support now, version is %s' % fisco)
-
-            os.makedirs(chain.data_dir())
-            shutil.copy(genesisjson, chain.data_dir() + '/')
-            shutil.copy(bootstrapnodesjson, chain.data_dir() + '/')
-
-            # create common dir
-            build.build_common_dir(chain, fisco)
-            # build install dir for every server
-            for node in cc.get_nodes():
-                try:
-                    build.expand_host_dir(chain, node, port, fisco)
-                except Exception as e:
-                    continue
-
-        except Exception as e:
-            if os.path.exists(chain.data_dir()):
-                shutil.rmtree(chain.data_dir())
-            logger.error(' expand failed, chain id is %s, chain version is %s, exception is %s.',
-                         chain.get_id(), chain.get_version(), e)
-        else:
-            consoler.info(' \t expand package for chain %s version %s success.',
-                          chain.get_id(), chain.get_version())
-
-    logger.info('expand end, cc is %s', cc)
+    except Exception as e:
+        if os.path.exists(chain.data_dir()):
+            shutil.rmtree(chain.data_dir())
+        logger.error(' expand failed, chain id is %s, chain version is %s, exception is %s.',
+                     chain_id, chain_version, e)
+        raise MCError(' expand failed, chain id is %s, chain version is %s, exception is %s' % (
+            chain_id, chain_version, e))
 
 def chain_expand(cfg, args):
     """expand operation 
@@ -127,7 +140,12 @@ def chain_expand(cfg, args):
             raise MCError(
                 ' parser config failed, invalid format, config is %s, exception is %s' % (cfg, e))
 
-        expand_cc(cc, fisco_path, genesisjson, bootstrapnodesjson)
+        if os.path.exists(cc.get_chain().data_dir()):
+            expand_on_exist_chain(cc)
+        else:
+            expand_on_nonexist_chain(cc, fisco_path, genesisjson, bootstrapnodesjson)
+            consoler.info(' expand install package for chain %s version %s success.', cc.get_chain().get_id(), cc.get_chain().get_version())
 
     except MCError as me:
         consoler.error(me)
+        logger.error(me)
