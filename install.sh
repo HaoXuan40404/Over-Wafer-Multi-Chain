ff4c9b4d7ca7c2484d0a47ed97fe82b80077f6ba
octopuswang <octopuswang@webank.com>
2018-11-04 16:11:09 +0800

    update

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 # OWMC init and install shell.
-set -x
+
+set -e
 
 dirpath="$(cd "$(dirname "$0")" && pwd)"
 cd $dirpath
 
 function alarm()
 {
-    echo "\033[1;31m $1 \033[0m"; exit 1;
+    local content=${1}
+    echo -e "\033[31m ${content} \033[0m"
 }
 
-function check_file()
+# check python enviroment.
+function check_python()
 {
-    local file=$1
-    if [ -z $file ];then
-        alarm " not found file $file."
+    python=$1
+    py_version=$($python -V 2>/dev/null | awk {'print $2'} | awk -F. {' print $1"."$2"."$3 '}
+3.6.6)
+    if [ ! -z '${py_version}' ];then
+        # echo " python path is ${python}, version is ${py_version}"
+        return 0
     fi
+    return 1
 }
 
+# check if soft installed.
 function check_install()
 {
     type $1 >/dev/null 2>&1
@@ -26,6 +34,7 @@ function check_install()
     }
 }
 
+# check sudo permission
 function sudo_permission_check() 
 {
     sudo echo -n " "
@@ -42,28 +51,6 @@ function format()
     find . -name "web3sdk"|  while read LINE; do chmod +x $LINE; dos2unix $LINE 2>/dev/null ; done
 }
 
-DEFAULT_INSTALL_DIR='/usr/local/'
-DEFAULT_PYTHON_ENV='/usr/bin/python'
-function OWMC_install()
-{
-    local install_dir=$1
-    if [ -z "${install_dir}" ];then
-        install_dir=${DEFAULT_INSTALL_DIR}
-    fi
-
-    local python_env=${DEFAULT_PYTHON_ENV}
-    echo " OWMC install dictionary is ${install_dir}"
-    echo " OWMC python env is ${python_env}"
-
-    if [ -d ${install_dir}/OWMC ];then
-        alarm " ${install_dir} already exist, OWMC maybe has been installed, --force can be set to install again. "
-    fi
-
-    sudo mkdir ${install_dir}/OWMC
-    sudo cp $dirpath/*  ${install_dir}/OWMC/
-
-}
-
 function deps_install()
 {
 
@@ -71,20 +58,79 @@ function deps_install()
 
 function deps_check()
 {
-    check_install dos2unix 
-    check_install envsubst
+
+}
+
+#OWMC install dir, default '/usr/local/'
+install_dir="/usr/local/"
+python_env='/usr/bin/python'
+force="false"
+gm="false"
+OWMC="/usr/bin/OWMC"
+function install()
+{
+    # sudo permission check
+    sudo_permission_check
+    
+    # params check
+    if ! $(check_python);then
+        alarm " Not invalid python path."; exit 1;
+    fi
+
+    if [ -f $OWMC ];then
+        if force == "true";then
+            sudo rm -rf $OWMC
+        else
+            alarm " $OWMC already install, set '-f' to force install." ; exit 1;
+        fi
+    fi
+
+    if [ -d ${install_dir}/OWMC ];then
+        if force == "true";then
+            sudo rm -rf ${install_dir}/OWMC
+        else
+            alarm " $OWMC already install, set '-f' to force install." ; exit 1;
+        fi
+    fi
+
+    echo " OWMC install dir is ${install_dir}"
+    echo " OWMC python env is ${python_env}"
+
+    sudo mkdir -p ${install_dir}/OWMC
+    sudo cp $dirpath/*  ${install_dir}/OWMC/
+
+    deps_install
+}
+
+function help() 
+{
+    echo "Usage:"
+    echo "Optional:"
+    echo "    -d  <dir>           The dir of OWMC will be install. (default: /usr/loca/)"
+    echo "    -p  <path>          The python path. (default: /usr/bin/python) "
+    echo "    -g 			      Install guomi deps. (default: not install guomi deps.)"
+    echo "    -f                  Install OWMC even if it has been installed."
+    echo "    -h                  This help"
+    echo "Example:"
+    echo "    bash install.sh "
+    echo "    bash install.sh -d /usr/local -p /usr/bin/python -f -g "
+    exit 0
 }
 
 function main()
 {
-    #
-    sudo_permission_check
-    # dos2unix to json and sh file
-    format
-    # install all OWMC deps soft
-    deps_install
-    # check if OWMC deps soft install success
-    deps_check
-    # 
-    OWMC_install
+    while getopts "d:p:gfh" option;do
+        case $option in
+        d) install_dir=$OPTARG;;
+        p) python_env=$OPTARG;;
+        f) force="true"
+        g) gm="true"
+        h) help;;
+        esac
+    done
+
+    # install
+    install
 }
+
+main
