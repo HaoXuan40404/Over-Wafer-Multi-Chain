b7accfba4744ff69caae76ef735c4f8d96ab288e
octopuswang <octopuswang@webank.com>
2018-11-04 14:33:15 +0800

    update

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def Usage():
                                                             ), help='list published packages info.')
     mgr_group.add_argument('--lshost', nargs='+', metavar=('host_ip'),
                            help='ls published packages\' host')
-    mgr_group.add_argument('-f', '--force', action='store_true',
+    mgr_group.add_argument('--force', action='store_true',
                            help='follow --publish/-p, all package of chain will be republished')
 
     tools_group = parser.add_argument_group(
@@ -98,9 +98,9 @@ def Usage():
     tools_group.add_argument('--envcheck', nargs='+', metavar=('all or host_ip'),
                              help='check build environment')
 
-    tools_group.add_argument('-d', '--docmd', nargs=2, metavar=(' host ip or chain id or \'all\'',
+    tools_group.add_argument('--docmd', nargs=2, metavar=(' host ip or chain id or \'all\'',
                                                                 'shell cmd or shell file, eg ： \'ls -lt\'、test.sh'), help='execute a shell command or shell file on remote server')
-    tools_group.add_argument('-P', '--pushfile', nargs=3, metavar=('host ip or chain id or \'all\'',
+    tools_group.add_argument('--pushfile', nargs=3, metavar=('host ip or chain id or \'all\'',
                                                                    'file or dir to be push.', 'dst dir.'), help='push one file or dir to remote server.')
     tools_group.add_argument('--chainca', nargs=1, metavar=('chain_dir',),
                              help='generate root cert')
