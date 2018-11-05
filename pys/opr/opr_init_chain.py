88fc14acc13512a85c24f3640d987da98f18ba27
octopuswang <octopuswang@webank.com>
2018-11-05 20:53:19 +0800

    update

--- pys/opr/opr_init_chain.py
+++ pys/opr/opr_init_chain.py
@@ -3,19 +3,8 @@ import os
 import json
 import shutil
 from pys import path
-from pys import ansible, utils
-from pys.chain.meta import *
-from pys.chain.package import AllChain
-from pys.chain.package import ChainVers
-from pys.chain.package import VerHosts
-from pys.chain.package import HostNodeDirs
-from pys.chain.port import AllChainPort
-from pys.log import logger
-from pys.log import consoler
-from pys.chain import data
-from pys.chain import package
 
 def init_chain(hosts_conf):
  
-    os.system('sudo bash ./scripts/hostsname.sh' + ' ' + hosts_conf)
+    os.system('sudo bash ' + path.get_path() + '/scripts/hostsname.sh' + ' ' + hosts_conf)
     os.system('bash ./scripts/ssh_copy_add.sh')
