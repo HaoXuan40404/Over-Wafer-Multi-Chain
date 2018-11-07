26af5660b433df44877a96c1846e07e6c703f7cd
haoxuan40404 <444649358@qq.com>
2018-11-07 19:57:53 +0800

    update ca

---
+++ scripts/ca/cts.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+###########################################################
+# create by feinenxiang
+###########################################################
+
+EXIT_CODE=-1
+
+check_env() {
+    version=`openssl version 2>&1 | grep 1.0.2`
+    [ -z "$version" ] && {
+        echo "please install openssl 1.0.2k-fips!"
+        #echo "please install openssl 1.0.2 series!"
+        #echo "download openssl from https://www.openssl.org."
+        echo "use \"openssl version\" command to check."
+        exit $EXIT_CODE
+    }
+}
+check_env
+
+check_java() {
+    ver=`java -version 2>&1 | grep version | grep 1.8`
+    tm=`java -version 2>&1 | grep "Java(TM)"`
+    [ -z "$ver" -o -z "$tm" ] && {
+        echo "please install java Java(TM) 1.8 series!"
+        echo "use \"java -version\" command to check."
+        exit $EXIT_CODE
+    }
+
+    which keytool >/dev/null 2>&1
+    [ $? != 0 ] && {
+        echo "keytool command not exists!"
+        exit $EXIT_CODE
+    }
+}
+
+usage() {
+printf "%s\n" \
+"usage command gen_chain_cert chaindir|
+              gen_agency_cert chaindir agencydir|
+              gen_node_cert agencydir nodedir|
+              gen_sdk_cert agencydir sdkdir|
+              help"
+}
+
+getname() {
+    local name="$1"
+    if [ -z "$name" ]; then
+        return 0
+    fi
+    [[ "$name" =~ ^.*/$ ]] && {
+        name="${name%/*}"
+    }
+    name="${name##*/}"
+    echo "$name"
+}
+
+check_name() {
+    local name="$1"
+    local value="$2"
+    [[ "$value" =~ ^[a-zA-Z0-9._-]+$ ]] || {
+        echo "$name name [$value] invalid, it should match regex: ^[a-zA-Z0-9._-]+\$"
+        exit $EXIT_CODE
+    }
+}
+
+file_must_exists() {
+    if [ ! -f "$1" ]; then
+        echo "$1 file does not exist, please check!"
+        exit $EXIT_CODE
+    fi
+}
+
+dir_must_exists() {
+    if [ ! -d "$1" ]; then
+        echo "$1 DIR does not exist, please check!"
+        exit $EXIT_CODE
+    fi
+}
+
+dir_must_not_exists() {
+    if [ -e "$1" ]; then
+        echo "$1 DIR exists, please clean old DIR!"
+        exit $EXIT_CODE
+    fi
+}
+
+gen_chain_cert() {
+    path="$2"
+    name=12345
+    chaindir=$path
+    if [ ! -d $path ]; then
+        mkdir -p $chaindir
+    fi
+    openssl genrsa -out $chaindir/ca.key 2048
+    openssl req -new -x509 -days 3650 -subj "/CN=$name/O=fiscobcos/OU=chain" -key $chaindir/ca.key -out $chaindir/ca.crt
+    cp cert.cnf $chaindir
+
+    if [ $? -eq 0 ]; then
+        echo "build chain ca succussful!"
+    else
+        echo "please input at least Common Name!"
+    fi
+}
+
+gen_agency_cert() {
+    chain="$2"
+    agencypath="$3"
+    name="$4"
+
+    dir_must_exists "$chain"
+    file_must_exists "$chain/ca.key"
+    check_name agency "$name"
+    agencydir=$agencypath'/'$name
+    dir_must_not_exists "$agencydir"
+    mkdir -p $agencydir
+
+    openssl genrsa -out $agencydir/agency.key 2048
+    openssl req -new -sha256 -subj "/CN=$name/O=fiscobcos/OU=agency" -key $agencydir/agency.key -config $chain/cert.cnf -out $agencydir/agency.csr
+    openssl x509 -req -days 3650 -sha256 -CA $chain/ca.crt -CAkey $chain/ca.key -CAcreateserial\
+        -in $agencydir/agency.csr -out $agencydir/agency.crt  -extensions v4_req -extfile $chain/cert.cnf
+    
+    cp $chain/ca.crt $chain/cert.cnf $agencydir/
+    cp $chain/ca.crt $agencydir/ca-agency.crt
+    more $agencydir/agency.crt | cat >>$agencydir/ca-agency.crt
+    rm -f $agencydir/agency.csr
+
+    echo "build $name agency cert successful!"
+}
+
+gen_cert_secp256k1() {
+    capath="$1"
+    certpath="$2"
+    name="$3"
+    type="$4"
+    openssl ecparam -out $certpath/${type}.param -name secp256k1
+    openssl genpkey -paramfile $certpath/${type}.param -out $certpath/${type}.key
+    openssl pkey -in $certpath/${type}.key -pubout -out $certpath/${type}.pubkey
+    openssl req -new -sha256 -subj "/CN=${name}/O=fiscobcos/OU=${type}" -key $certpath/${type}.key -config $capath/cert.cnf -out $certpath/${type}.csr
+    openssl x509 -req -days 3650 -sha256 -in $certpath/${type}.csr -CAkey $capath/agency.key -CA $capath/agency.crt\
+        -force_pubkey $certpath/${type}.pubkey -out $certpath/${type}.crt -CAcreateserial -extensions v3_req -extfile $capath/cert.cnf
+    openssl ec -in $certpath/${type}.key -outform DER | tail -c +8 | head -c 32 | xxd -p -c 32 | cat >$certpath/${type}.private
+    rm -f $certpath/${type}.csr
+}
+
+gen_node_cert() {
+    if [ "" = "`openssl ecparam -list_curves 2>&1 | grep secp256k1`" ]; then
+        echo "openssl don't support secp256k1, please upgrade openssl!"
+        exit $EXIT_CODE
+    fi
+
+    agpath="$2"
+    agency=`getname "$agpath"`
+    nodepath="$3"
+    node="$4"
+    ndpath=$nodepath'/'$node
+    dir_must_exists "$agpath"
+    file_must_exists "$agpath/agency.key"
+    check_name agency "$agency"
+    dir_must_not_exists "$ndpath"
+    check_name node "$node"
+
+    mkdir -p $ndpath
+    gen_cert_secp256k1 "$agpath" "$ndpath" "$node" node
+    #nodeid is pubkey
+    openssl ec -in $ndpath/node.key -text -noout | sed -n '7,11p' | tr -d ": \n" | awk '{print substr($0,3);}' | cat >$ndpath/node.nodeid
+    openssl x509 -serial -noout -in $ndpath/node.crt | awk -F= '{print $2}' | cat >$ndpath/node.serial
+    cp $agpath/ca.crt $agpath/agency.crt $ndpath
+
+    cd $ndpath
+    nodeid=`cat node.nodeid | head`
+    serial=`cat node.serial | head`
+    cat >node.json <<EOF
+{
+ "id":"$nodeid",
+ "name":"$node",
+ "agency":"$agency",
+ "caHash":"$serial"
+}
+EOF
+	cat >node.ca <<EOF
+{
+ "serial":"$serial",
+ "pubkey":"$nodeid",
+ "name":"$node"
+
+EOF
+
+    echo "build $node node cert successful!"
+}
+
+read_password() {
+    read -se -p "Enter password for keystore:" pass1
+    echo
+    read -se -p "Verify password for keystore:" pass2
+    echo
+    [[ "$pass1" =~ ^[a-zA-Z0-9._-]{6,}$ ]] || {
+        echo "password invalid, at least 6 digits, should match regex: ^[a-zA-Z0-9._-]{6,}\$"
+        exit $EXIT_CODE
+    }
+    [ "$pass1" != "$pass2" ] && {
+        echo "Verify password failure!"
+        exit $EXIT_CODE
+    }
+    mypass=$pass1
+}
+
+gen_sdk_cert() {
+    check_java
+
+    agency="$2"
+    sdkph="$3"
+    sdk="sdk"
+    dir_must_exists "$agency"
+    file_must_exists "$agency/agency.key"
+    sdkpath=$sdkph'/'$sdk
+    dir_must_not_exists "$sdkpath"
+
+    mkdir -p $sdkpath
+    gen_cert_secp256k1 "$agency" "$sdkpath" "$sdk" sdk
+    cp $agency/ca-agency.crt $sdkpath/ca.crt
+    
+    mypass="123456"
+    openssl pkcs12 -export -name client -passout "pass:$mypass" -in $sdkpath/sdk.crt -inkey $sdkpath/sdk.key -out $sdkpath/keystore.p12
+    keytool -importkeystore -srckeystore $sdkpath/keystore.p12 -srcstoretype pkcs12 -srcstorepass $mypass\
+        -destkeystore $sdkpath/client.keystore -deststoretype jks -deststorepass $mypass -alias client 2>/dev/null 
+
+    echo "build $sdk sdk cert successful!"
+}
+
+case "$1" in
+gen_chain_cert)
+    gen_chain_cert "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+    ;;
+gen_agency_cert)
+    gen_agency_cert "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+    ;;
+gen_node_cert)
+    gen_node_cert "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+    ;;
+gen_sdk_cert)
+    gen_sdk_cert "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+    ;;
+help)
+    usage
+    ;;
+*)
+    usage
+esac
